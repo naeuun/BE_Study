b8364a7b92173f9c7bdece7bfcf6746c2e6dabbe
naeun <1210naeun@gmail.com>
2025-09-08 02:18:33 +0900

    실습 3-5

--- chap03/src/main/java/com/ll/chap03/HomeController.java
+++ chap03/src/main/java/com/ll/chap03/HomeController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class HomeController {
     @GetMapping("a")
@@ -18,14 +22,122 @@ public class HomeController {
     @GetMapping("b")
     @ResponseBody
     public String plus(
-            @RequestParam("a") String num1Str,
-            @RequestParam("b") String num2Str
+            @RequestParam("a") int num1,
+            @RequestParam("b") int num2,
+            @RequestParam(name="c", defaultValue = "10") int num3
     ){
-        int num1 = Integer.parseInt(num1Str);
-        int num2 = Integer.parseInt(num2Str);
-
         System.out.println("num1 = " + num1);
         System.out.println("num2 = " + num2);
-        return "a + b = %d".formatted(num1+num2);
+        System.out.println("num3 = " + num3);
+        return "a + b + c = %d".formatted(num1+num2+num3);
+    }
+    @GetMapping("c")
+    @ResponseBody
+    public String c(
+            boolean married
+    ){
+        return married ? "결혼" : "미혼";
+    }
+    @GetMapping("d")
+    @ResponseBody
+    public String d(
+            Boolean married
+    ){
+        if ( married == null ) return "정보를 입력하세요.";
+        return married ? "결혼" : "미혼";
+    }
+
+    public static class Person {
+
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+    @GetMapping("person1")
+    @ResponseBody
+    public String person(
+            String name,
+            int age
+    ){
+        Person person = new Person(name,age);
+
+        return person.toString();
+    }
+    @GetMapping("person2")
+    @ResponseBody
+    public String person(
+            Person person
+    ){
+        return person.toString();
+    }
+    @GetMapping("e")
+    @ResponseBody
+    public int e(){
+        int age = 100;
+        return age;
+    }
+    @GetMapping("f")
+    @ResponseBody
+    public boolean f(){
+        boolean married = false;
+
+        return married;
+    }
+    @GetMapping("g")
+    @ResponseBody
+    public Person g(){
+        Person person = new Person("Paul",22);
+        return person;
+    }
+    @GetMapping("h")
+    @ResponseBody
+    public int[] h() {
+        int[] arr = new int[]{10, 20, 30};
+
+        return arr;
+    }
+    @GetMapping("i")
+    @ResponseBody
+    public List<Integer> i() {
+        List<Integer> arr = List.of(10,20,30);
+        return arr;
+    }
+    @GetMapping("j")
+    @ResponseBody
+    public Map<String, Object> j() {
+        Map<String, Object> person = new HashMap<>();
+
+        person.put("age", 22);
+        person.put("name", "Paul");
+
+        return person;
     }
 }
