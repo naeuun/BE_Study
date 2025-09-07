6c60cc860647e5d332dafc2669cb159721b35472
naeun <1210naeun@gmail.com>
2025-09-07 21:59:32 +0900

    실습 2-7

--- chap02/src/main/java/com/ll/App.java
+++ chap02/src/main/java/com/ll/App.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -7,17 +9,22 @@ public class App {
         System.out.println("할 일 관리 앱, 시작");
 
         try( Scanner scanner = new Scanner(System.in) ) {
+            List<Todo> todos = new ArrayList<>();
+            long todosLastId = 0;
+
             while( true ){
                 System.out.print("명령) ");
                 String cmd = scanner.nextLine().trim();
 
                 if (cmd.equals("exit")) break;
                 else if (cmd.equals("add")) {
-                    long id = 1;
+                    long id = todosLastId + 1;
                     System.out.print("할 일 : ");
                     String content = scanner.nextLine().trim();
 
                     Todo todo = new Todo(id, content);
+                    todos.add(todo);
+                    todosLastId++;
 
                     System.out.println(todo.getId());
 
