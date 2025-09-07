1f9e3c7b471b21684a3a92dc8f1b35551f61d937
naeun <1210naeun@gmail.com>
2025-09-07 18:09:56 +0900

    실습 2-6

--- chap02/src/main/java/com/ll/Main.java
+++ chap02/src/main/java/com/ll/Main.java
@@ -4,6 +4,7 @@ package com.ll;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("안녕!");
+        App app = new App();
+        app.run();
     }
 }
