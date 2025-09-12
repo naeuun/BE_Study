77e315b6806a63c9aab823ff0acd47be3a2955cc
naeun <1210naeun@gmail.com>
2025-09-13 00:02:04 +0900

    test

--- chap03/src/main/java/com/ll/chap03/ComponentE.java
+++ chap03/src/main/java/com/ll/chap03/ComponentE.java
@@ -2,6 +2,6 @@ package com.ll.chap03;
 
 public class ComponentE {
     public ComponentE(){
-        System.out.println("ComponentE 객체 생성됨");
+        System.out.println("ComponentE 객체 생성됨!");
     }
 }
