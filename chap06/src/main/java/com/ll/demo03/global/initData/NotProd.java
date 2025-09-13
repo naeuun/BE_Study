b5d78024907cddcc02da32a9731f32da95c493d3
naeun <1210naeun@gmail.com>
2025-09-14 00:42:20 +0900

    실습 6-8

--- chap06/src/main/java/com/ll/demo03/global/initData/NotProd.java
+++ chap06/src/main/java/com/ll/demo03/global/initData/NotProd.java
@@ -3,10 +3,12 @@ package com.ll.demo03.global.initData;
 import com.ll.demo03.domain.article.article.entity.Article;
 import com.ll.demo03.domain.article.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 
 // !prod : dev or test
 @Profile("!prod")
@@ -14,24 +16,31 @@ import org.springframework.context.annotation.Profile;
 @RequiredArgsConstructor
 public class NotProd {
     private final ArticleRepository articleRepository;
+
     @Bean
     public ApplicationRunner initNotprod() {
-        return args -> {
-            if( articleRepository.count() >0 ) return;
+        return  new ApplicationRunner() {
+            @Override
+            @Transactional
+            public void run(ApplicationArguments args) throws Exception {
+                if( articleRepository.count() >0 ) return;
 
-            Article article1 = Article.builder()
-                    .title("제목 1")
-                    .body("내용 ")
-                    .build();
+                Article article1 = Article.builder()
+                        .title("제목 1")
+                        .body("내용 ")
+                        .build();
 
-            Article article2 = Article.builder()
-                    .title("제목 1")
-                    .body("내용 2")
-                    .build();
-            articleRepository.save(article1);
-            articleRepository.save(article2);
+                Article article2 = Article.builder()
+                        .title("제목 1")
+                        .body("내용 2")
+                        .build();
+                articleRepository.save(article1);
+                articleRepository.save(article2);
 
-            articleRepository.delete(article1);
+                article2.setTitle("제목!");
+
+                articleRepository.delete(article1);
+            }
         };
     }
 }
