ef668a8fc84ea11a8b578916b5344586ecb719fc
naeun <1210naeun@gmail.com>
2025-09-13 14:48:48 +0900

    setting

---
+++ chap06/src/main/java/com/ll/demo03/HomeController.java
@@ -0,0 +1,26 @@
+package com.ll.demo03;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @Value("${custom.site.name}")
+    private String siteName;
+    @Value("${custom.secret.key}")
+    private String secretKey;
+
+    @GetMapping("/")
+    @ResponseBody
+
+    public String ShowMain(){ return "Hello World! " + siteName; }
+
+    @GetMapping("/secretKey")
+    @ResponseBody
+    public String ShowSecretKey(){
+        return "Secret Key : " + secretKey;
+
+    }
+}
