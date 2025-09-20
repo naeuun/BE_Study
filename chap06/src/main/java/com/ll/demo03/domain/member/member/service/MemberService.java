8ea0a404ac4a7c45ab2c29ebc6dd9bfd5542d919
naeun <1210naeun@gmail.com>
2025-09-21 06:58:06 +0900

    실습 6-15

--- chap06/src/main/java/com/ll/demo03/domain/member/member/service/MemberService.java
+++ chap06/src/main/java/com/ll/demo03/domain/member/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.ll.demo03.domain.member.member.service;
 
 import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.repository.MemberRepository;
+import com.ll.demo03.global.exceptions.GlobalException;
 import com.ll.demo03.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,14 +15,9 @@ public class MemberService{
     private final MemberRepository memberRepository;
 
     public RsData<Member> join(String username, String password, String nickname) {
-        /* findByUsername(username).ifPresent(m -> {
-            throw new IllegalArgumentException("이미 사용중인 username 입니다.");
-        }); */
-
-        boolean present = findByUsername(username).isPresent();
-        if (present) {
-            return RsData.of("400-1", "이미 존재하는 아이디입니다.", Member.builder().build());
-        }
+         findByUsername(username).ifPresent( ignored -> {
+            throw new GlobalException("400-1", "%s는 이미 사용중인 username 입니다.".formatted(username));
+        });
 
         Member member = Member.builder()
                 .username(username)
--- chap06/src/main/java/com/ll/demo03/global/initData/NotProd.java
+++ chap06/src/main/java/com/ll/demo03/global/initData/NotProd.java
@@ -4,6 +4,7 @@ import com.ll.demo03.domain.article.article.entity.Article;
 import com.ll.demo03.domain.article.article.service.ArticleService;
 import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.service.MemberService;
+import com.ll.demo03.global.exceptions.GlobalException;
 import com.ll.demo03.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,9 +43,13 @@ public class NotProd {
         Member member1 = memberService.join("user1", "1234", "유저 1").getData();
         Member member2 = memberService.join("user2", "1234", "유저 2").getData();
 
-        RsData<Member> joinRs = memberService.join("user2", "1234", "유저 2");
-        System.out.println(" joinRs.getMsg() : " + joinRs.getMsg());
-        System.out.println(" joinRs.StatusCode() : " + joinRs.getStatusCode());
+        try{
+            RsData<Member> joinRs = memberService.join("user2", "1234", "유저 2");
+        }
+        catch (GlobalException e) {
+            System.out.println("e.getMessage() : " + e.getRsData().getMsg());
+            System.out.println("e.getStatusCode() : " + e.getRsData().getStatusCode());
+        }
 
         //RsData<Article> write = articleService.write("제목 1", "내용 1");
         //Article article1 = write.getData();
