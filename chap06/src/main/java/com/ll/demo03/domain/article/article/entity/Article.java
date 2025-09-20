0f0ee8455a8f33ff85125206b058269534ac6989
naeun <1210naeun@gmail.com>
2025-09-21 08:06:57 +0900

    실습 6-17

--- chap06/src/main/java/com/ll/demo03/domain/article/article/entity/Article.java
+++ chap06/src/main/java/com/ll/demo03/domain/article/article/entity/Article.java
@@ -1,30 +1,19 @@
 package com.ll.demo03.domain.article.article.entity;
 
-import jakarta.persistence.*;
+import com.ll.demo03.global.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 @Builder
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class) // CreatedDate, LastModifiedDate 작동을 위해 필요
-public class Article {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Article extends BaseTime {
     private String title;
     @Column(columnDefinition = "TEXT")
     private String body;
--- chap06/src/main/java/com/ll/demo03/domain/member/member/entity/Member.java
+++ chap06/src/main/java/com/ll/demo03/domain/member/member/entity/Member.java
@@ -1,31 +1,18 @@
 package com.ll.demo03.domain.member.member.entity;
 
-import jakarta.persistence.*;
+import com.ll.demo03.global.jpa.entity.BaseTime;
+import jakarta.persistence.Entity;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 @Builder
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class) // CreatedDate, LastModifiedDate 작동을 위해 필요
-public class Member {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private Long id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
-    @Column(unique = true)
+public class Member extends BaseTime {
     private String username;
     private String password;
     private String nickname;
