5789dfe58268309c43bd7b500593292d1467c78d
naeun <1210naeun@gmail.com>
2025-09-30 00:55:16 +0900

    실습 7-13

---
+++ terraform-project-1/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+    description = "Prefix for all resource"
+    default     = "dev"
+}
+
+variable "region" {
+    description = "region"
+    default     = "ap-northeast-2"
+}
+
+variable "nick_name" {
+    description = "nickname"
+    default     = "naeuun"
+}
