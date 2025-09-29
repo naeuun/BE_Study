80adc3f51fbaa36180c1b61c40828f68ac6810ad
naeun <1210naeun@gmail.com>
2025-09-30 02:32:53 +0900

    실습 8-3

--- chap06/src/main/java/com/ll/demo03/domain/member/member/controller/MemberController.java
+++ chap06/src/main/java/com/ll/demo03/domain/member/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.ll.demo03.domain.member.member.entity.Member;
 import com.ll.demo03.domain.member.member.service.MemberService;
 import com.ll.demo03.global.exceptions.GlobalException;
 import com.ll.demo03.global.rsData.RsData;
+import com.ll.demo03.standard.dto.util.Ut;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,13 +20,13 @@ public class MemberController {
     public RsData join(
         String username, String password, String nickname
             ){
-        if (username == null || username.isBlank()) {
+        if (Ut.str.isBlank(username)) {
             throw new GlobalException("400-1", "username(을)를 입력해주세요.");
         }
-        if (password == null || password.isBlank()) {
+        if (Ut.str.isBlank(password)) {
             throw new GlobalException("400-2", "password(을)를 입력해주세요.");
         }
-        if (nickname == null || nickname.isBlank()) {
+        if (Ut.str.isBlank(nickname)) {
             throw new GlobalException("400-3", "nickname(을)를 입력해주세요.");
         }
         RsData<Member> joinRs = memberService.join(username, password, nickname);
